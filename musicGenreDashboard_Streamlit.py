8f1d03b3541ea347a8fc0cf73e9a0526fc42855e
GeanKabs <GeanKabs@users.noreply.github.com>
2022-07-27 22:10:51 +0300

    Update musicGenreDashboard_Streamlit.py

--- musicGenreDashboard_Streamlit.py
+++ musicGenreDashboard_Streamlit.py
@@ -42,7 +42,7 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title='
 # Loading the available data and overview
 path = "PinkPanther30.wav"
 st.sidebar.header('Genre Classification')
-st.sidebar.write('''This a Music Genre Classification App, that tries to predict which genre a music belongs to. ''')
+st.sidebar.write('''This a Music Genre Classification App, that tries to predict which genre a music file belongs to. ''')
 data = st.sidebar.file_uploader("Upload Dataset", type=['wav', 'au', 'mp3'])
 
 # source for this code is at: https://blog.jcharistech.com/2021/01/21/how-to-save-uploaded-files-to-directory-in-streamlit-apps/
